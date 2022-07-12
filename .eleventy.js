d23f1d274a4ce122f1b86a815eb6c68bb9dc8a34
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-07-11 21:55:37 -0400

    Added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,19 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/fonts/");
+  eleventyConfig.addPassthroughCopy("./src/favicons/");
+
+
 
   return {
     dir: {
