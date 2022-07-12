d1d507bbbb6b6f0326ef34accbfa2fa2fd969f86
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-07-12 10:41:48 -0400

    Remove fonts dir as pass-through copy.
    Fonts were removed in commit 312a1e226e4eaf4a15cabe922e9f58562a6933a5

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPassthroughCopy("./src/css/");
-    eleventyConfig.addPassthroughCopy("./src/fonts/");
     eleventyConfig.addPassthroughCopy("./src/favicons/");
 
     return {
